dc1b46670f6311abdc32e199ce38c464c2ac95d2
David Naimi <d88naimi@gmail.com>
2020-03-27 08:08:33 -0700

    added psuedo code and defined vars

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-let gameHome =document.querySelector('.game-home')
-console.log(gameHome);
-// make an array of images to loop through 
-//make vars for the game
-// init game 
+let gameHome =document.querySelector('.game-home');
+let firstBox;
+let secondBox;
+let preventSelected = false
 
-let images = ["corvette", "ferrari", "lambo", "mb"]
+let images = ["corvette", "ferrari", "lambo", "mb"];
+
+// init game 
+init();
+
+function init(){
+    render();
+    handler();
+}
 //shuffle array images function
-//   const shuffle = images[Math.floor(Math.random() * images.length)];
 
 //   console.log(shuffle);
-
   function randomItemFromArray(arr, not) {
     const item = arr[Math.floor(Math.random() * arr.length)];
     if (item === not) {
@@ -19,25 +24,51 @@ let images = ["corvette", "ferrari", "lambo", "mb"]
     return item;
   }
 console.log(randomItemFromArray(images));
+//render function 
 
+function render(){
+// loop through images 
+// place images into container
 images.forEach(i => {
 var html = `
     <div class="cell small-3 box">
-        <button><img src="./images/${i}.jpg" alt="car"></button></div>
+        <button data-car=${i} class="butBox"><img src="./images/${i}.jpg" alt="car"></button></div>
     </div>
+    <div class="cell small-3 box">
+        <button data-car=${i} class="butBox"><img src="./images/${i}.jpg" alt="car"></button></div>
+    </div>
+    
 `;    
+// append images to container
 gameHome.insertAdjacentHTML("afterbegin", html);
 });
+}
 
-
-
-//render function 
-// loop through images 
-// place images into container
-// append images to container
 
 // handler function
-// compare firstselection value to second selection value 
+// check if anything is selected else assign clicked img to firstSelection
+// if preventSelected is true 
+// assign next img selected to secondSelection
+// if firstSelection is and secondSelection is true
+// check if firstSelection === secondSelection 
+// leave show class on 
+// else remove show class 
+function handler(){
+  $(document).on("click", function(event) {
+    const checkIfSelected = event.target.closest(".butBox");
+    preventSelected = true;
+    if (preventSelected) {
+      firstBox = checkIfSelected.getAttribute("data-car");
+      console.log(firstBox);
+    } else {
+      console.log('no');
+    }
+    
+
+    // $("img").toggleClass("show");
+
+  });
+} 
 // if correct leave facing up 
 //else flip the cards back over timer may be needed 
 
